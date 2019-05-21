e5c77597bfab938d1ba8074190d0fb6614cc4c5e
Rix <rixwoodling@gmail.com>
2019-05-21 04:30:30 -0700

    Update omxpocket.sh

--- omxpocket.sh
+++ omxpocket.sh
@@ -18,24 +18,20 @@ m=50    # offset margin setting (plus or minus to margin)
 f=mp4   # file type 
 
 # gather video dimensions and scale down
-w0=$(exiftool -s3 -ImageWidth *.${f}) | w=$((w0 / (100/p)))
-echo $w
-h0=$(exiftool -s3 -ImageHeight *.${f}) | h=$((h0 / (100/p)))
-echo $h
+w=$(exiftool -s3 -ImageWidth *.${f})
+w=$((w*p/100))
+h=$(exiftool -s3 -ImageHeight *.${f})
+h=$((h*p/100))
+
 # gather monitor dimensions
 x=$(xdpyinfo | grep dimensions | sed -e 's/x.*//' -e 's/[^0-9]*//g')
-echo $x
 y=$(xdpyinfo | grep dimensions | sed 's/[^x]*.//' | awk '{print $1}')
-echo $y
+
 # move scaled video down toward bottom-right of screen
 x1=$((x - w - m))
-echo $x1
 y1=$((y - h - m))
-echo $y1
 x2=$((x - m))
-echo $x2
 y2=$((y - m))
-echo $y2
 # launch omxplayer with coordinets already worked out
 omxplayer -o hdmi --win "$x1,$y1,$x2,$y2" *.$f
 
